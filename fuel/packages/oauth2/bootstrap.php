f0fb1f22b808c5479c6feae21925c64c152c8dc8
crynobone <crynobone@gmail.com>
2011-12-15 08:51:27 +0800

    Update all files

--- fuel/packages/oauth2/bootstrap.php
+++ fuel/packages/oauth2/bootstrap.php
@@ -20,10 +20,13 @@ Autoloader::add_classes(array(
 	'OAuth2\\Provider_Google'  		=> __DIR__.'/classes/provider/google.php',
 	'OAuth2\\Provider_Github'  		=> __DIR__.'/classes/provider/github.php',
 	'OAuth2\\Provider_Unmagnify'  	=> __DIR__.'/classes/provider/unmagnify.php',
+	'OAuth2\\Provider_Windowslive'  => __DIR__.'/classes/provider/windowslive.php',
 	
 	'OAuth2\\Request'  				=> __DIR__.'/classes/request.php',
 	
 	'OAuth2\\Token'  				=> __DIR__.'/classes/token.php',
+	'OAuth2\\Token_Access'  		=> __DIR__.'/classes/token/access.php',
+	'OAuth2\\Token_Authorize'		=> __DIR__.'/classes/token/authorize.php',
 	
 ));
 
